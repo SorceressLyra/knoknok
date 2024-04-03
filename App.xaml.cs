7d3a529dbd9e9cdf065358f9e7d195b2ec4bdd9a
Lyra Mikkelsen <lyramikkelsen@gmail.com>
2024-04-03 20:55:26 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Media;
+using System.Windows;
+using Windows.Foundation.Collections;
+using Hardcodet.Wpf.TaskbarNotification;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+
+namespace knoknok;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private Mutex instanceMutex;
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        //bool createdNew = false;
+
+        //instanceMutex = new Mutex(true, "knoknok app instance", out createdNew);
+
+        //if (!createdNew)
+        //{
+        //    instanceMutex = null;
+        //    MessageBox.Show("Instance of knoknok already active");
+        //    Application.Current.Shutdown();
+        //    return;
+        //}
+    }
+    public App()
+    {
+        // Listen to notification activation
+        ToastNotificationManagerCompat.OnActivated += toastArgs =>
+        {
+            // Obtain the arguments from the notification
+            ToastArguments args = ToastArguments.Parse(toastArgs.Argument);
+
+            // Obtain any user input (text boxes, menu selections) from the notification
+            ValueSet userInput = toastArgs.UserInput;
+
+            // Need to dispatch to UI thread if performing UI operations
+            Application.Current.Dispatcher.Invoke(delegate
+            {
+                //Listen for the reply button
+                if (args["action"] == "reply")
+                {
+                    SoundPlayer player = new("Audio/confirm.wav");
+                    player.Play();
+                }
+            });
+        };
+    }
+
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (instanceMutex != null)
+            instanceMutex.ReleaseMutex();
+
+        base.OnExit(e);
+    }
+
+    void AppExit(object sender, ExitEventArgs e)
+    {
+        ToastNotificationManagerCompat.History.Clear();
+        ToastNotificationManagerCompat.Uninstall();
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System.Windows;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace knoknok;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private byte id;
+    private int port = 11000;
+    
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        //get id
+        Random rnd = new Random();
+        id = (byte)rnd.Next(1,255);
+        
+        //server thread
+        Thread serverThread = new Thread(() => Server(port, id));
+        //serverThread.IsBackground = true;
+        serverThread.Start();
+
+    }
+
+    private void Server(int port, byte id)
+    {
+        UdpClient udpServer = new UdpClient(port);
+
+        Debug.WriteLine("Starting server");
+        while (true)
+        {
+            var remoteEP = new IPEndPoint(IPAddress.Any, 11000); 
+            byte[] data = udpServer.Receive(ref remoteEP); // listen on port 11000
+            Debug.WriteLine("receive data from " + remoteEP.ToString());
+
+            //if (data.Length != 2)
+            //    return;
+            
+            //if(data[0] == (byte)255 && data[1] != id)
+            //    ReceiveKnock();
+        }
+    }
+
+    public void KnockButtonClick(object sender, EventArgs e)
+    {
+        var client = new UdpClient();
+        IPEndPoint ep = new IPEndPoint(IPAddress.Broadcast, 11000); // endpoint where server is listening
+        client.Connect(ep);
+
+        // send data
+        client.Send(new byte[] { 255, id }, 2);
+        Debug.WriteLine("Sending knock");
+        SendKnock();
+    }
+    
+    //Close to tray instead
+    public void HandleClosing(object sender, CancelEventArgs  e)
+    {
+        this.Visibility = Visibility.Hidden;
+        e.Cancel = true;
+    }
+    
+    private static void ReceiveKnock()
+    {
+        // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+        new ToastContentBuilder()
+            .AddText("Knock Knock!")
+            .AddText("I'm trying to get your attention")
+            .SetToastScenario(ToastScenario.Alarm)
+            .AddAudio(new Uri("ms-winsoundevent:Notification.Looping.Alarm2"), true)
+            .AddInlineImage(new Uri(Path.GetFullPath("images/kirby.gif")))
+            // Buttons
+            .AddButton(new ToastButton()
+                .SetContent("Understood!")
+                .AddArgument("action", "reply")
+                .SetBackgroundActivation())
+            .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+    }
+    
+    private static void SendKnock()
+    {
+        // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
+        new ToastContentBuilder()
+            .AddText("Knock Knock!")
+            .AddText("You sent a knock")
+            .AddAudio(new Uri("ms-winsoundevent:Notification.Reminder"))
+            .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 6 (or later), then your TFM must be net6.0-windows10.0.17763.0 or greater
+    }
+
+    private void CloseApplication(object sender, RoutedEventArgs e)
+    {
+        Application.Current.Shutdown();
+    }
+}
